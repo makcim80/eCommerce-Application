f5df6a352e9a174dce645ec464269491f3fb3c2a
Yuliya Shelenhouskaya <yulchik241@mail.ru>
2023-08-10 20:13:46 +0300

    feat: add new styles and create new classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-color': '#8EB2C6',
+        'login-color': '#222222',
+      },
+      backgroundImage: {
+        'main-background': "url('./assets/img/bg.png')",
+      },
+      height: {
+        '85h': '21rem',
+      },
+      screens: {
+        'sm-s': '640px',
+        'lg-l': '1024px',
+      },
+      inset: {
+        '50px': '50px',
+      },
+    },
   },
   plugins: [],
 };
