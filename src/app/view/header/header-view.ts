6e5e6cfdd29e8b4a20fc6ee325555401065e8eaf
makcim80 <makcim80@mail.ru>
2023-08-15 23:11:45 +0300

    feat: implement cursor pointer to logo

--- src/app/view/header/header-view.ts
+++ src/app/view/header/header-view.ts
@@ -31,6 +31,7 @@ export default class HeaderView extends View {
     this.view.addInnerElement(headerContainer);
 
     const logo = document.createElement(ListTags.IMG);
+    logo.classList.add(ListClasses.HEADER_LOGO);
     logo.setAttribute(ListAttributes.SRC, ListPaths.LOGO);
     logo.setAttribute(ListAttributes.ALT, ListOfValues.LOGO);
     headerContainer.addInnerElement(logo);
