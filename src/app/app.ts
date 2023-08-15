b3e8c9a0d1cc2d1e019c4b05db4e5c692334f457
makcim80 <makcim80@mail.ru>
2023-08-15 16:52:49 +0300

    feat: implement address bar navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,14 @@
+import Router from './router/router';
+import { Pages } from './util/enums/pages';
 import FooterView from './view/footer/footer-view';
 import HeaderView from './view/header/header-view';
 import MainView from './view/main/main-view';
+import View from './view/view';
+
+interface Route {
+  path: string;
+  callback: (param: string) => void;
+}
 
 export default class App {
   private header: HeaderView | null;
@@ -9,10 +17,14 @@ export default class App {
 
   private footer: FooterView | null;
 
+  private router: Router;
+
   constructor() {
     this.header = null;
     this.main = null;
     this.footer = null;
+    const routes = this.createRoutes();
+    this.router = new Router(routes);
     this.createView();
   }
 
@@ -26,5 +38,35 @@ export default class App {
       this.main.getHTMLElement() || '',
       this.footer.getHTMLElement() || '',
     );
+  }
+
+  public createRoutes(): Route[] {
+    return [
+      {
+        path: ``,
+        callback: async (): Promise<void> => {
+          const { default: LoginView } = await import('./view/main/form-login/login-view');
+          this.setContent(Pages.LOGIN, new LoginView());
+        },
+      },
+      {
+        path: `${Pages.LOGIN}`,
+        callback: async (): Promise<void> => {
+          const { default: LoginView } = await import('./view/main/form-login/login-view');
+          this.setContent(Pages.LOGIN, new LoginView());
+        },
+      },
+      {
+        path: `${Pages.REGISTRATION}`,
+        callback: async (): Promise<void> => {
+          const { default: RegistrationView } = await import('./view/main/form-registration/registration-view');
+          this.setContent(Pages.REGISTRATION, new RegistrationView());
+        },
+      },
+    ];
+  }
+
+  public setContent(page: string, view: View): void {
+    this.main?.setContent(view);
   }
 }
