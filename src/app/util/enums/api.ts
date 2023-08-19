26e05e9ea7b3e840b0cda7815c22d7655e06a4a4
makcim80 <makcim80@mail.ru>
2023-08-19 19:24:40 +0300

    feat: implement submit on login

--- src/app/util/enums/api.ts
+++ src/app/util/enums/api.ts
@@ -5,4 +5,7 @@ export const enum Api {
   CLIENT_ID_REG = 'NbNq_M9W-iNztQ90dnaK9Xfr',
   CLIENT_SECRET_REG = 'y8b0Kkvms41tkGDWS1EigwC8idr7jZwK',
   SCOPES_REG = 'view_published_products:project0108 view_project_settings:project0108 manage_my_profile:project0108 manage_my_business_units:project0108 manage_my_shopping_lists:project0108 manage_my_quote_requests:project0108 manage_customers:project0108 create_anonymous_token:project0108 manage_my_orders:project0108 view_products:project0108 view_categories:project0108 manage_my_quotes:project0108 manage_my_payments:project0108',
+  CLIENT_ID_LOG = 'Ox1jTqGRgRyP_U9FM9B8H8pX',
+  CLIENT_SECRET_LOG = 'M0h1EgcHUKIl2S5Py7UfYz5Krr-bwhrj',
+  SCOPES_LOG = 'view_published_products:project0108 view_project_settings:project0108 manage_my_profile:project0108 manage_my_business_units:project0108 manage_my_shopping_lists:project0108 manage_my_quote_requests:project0108 create_anonymous_token:project0108 manage_my_orders:project0108 view_products:project0108 view_categories:project0108 manage_my_quotes:project0108 manage_my_payments:project0108',
 }
