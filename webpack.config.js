abebb5c72b9c2bd044c4ad546bc55045b61471ff
Yuliya Shelenhouskaya <yulchik241@mail.ru>
2023-08-22 15:33:52 +0300

    feat: add netlify config

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,10 @@ const baseConfig = {
         {
           from: path.resolve(__dirname, './src/assets'),
           to: path.resolve(__dirname, 'dist/assets')
+        },
+        {
+          from: path.resolve(__dirname, 'netlify.toml'),
+          to: path.resolve(__dirname, 'dist')
         }
       ]
     })
