26f7ae5781cce91dafd112e08f84bf9bee632ede
Darya Kondrina <dashakondrina61299@gmail.com>
2023-09-01 18:42:25 +0400

    feat: save into component class ang guard product name, description, images

--- src/app/view/main/cat-details/cat-details-view.ts
+++ src/app/view/main/cat-details/cat-details-view.ts
@@ -1,4 +1,4 @@
-import { ClientResponse, ProductProjectionPagedQueryResponse } from '@commercetools/platform-sdk';
+import { ClientResponse, Image, ProductProjectionPagedQueryResponse } from '@commercetools/platform-sdk';
 import { ISource } from '../../../util/types';
 import { ListTags } from '../../../util/enums/list-tags';
 import { ListClasses } from '../../../util/enums/list-classes';
@@ -7,10 +7,44 @@ import Product from '../../../../components/product';
 import ElementCreator from '../../../util/element-creator';
 import { ListAttributes } from '../../../util/enums/list-attributes';
 
+interface CatDetailsViewErrors {
+  getResponseError: (err?: Error) => Error;
+  responseIsNull: () => Error;
+  nameNotExist: () => Error;
+  imagesNotExist: () => Error;
+  descriptionNotExist: () => Error;
+}
+
+const Errors: CatDetailsViewErrors = {
+  getResponseError: (err) => {
+    return new Error(`Error in CatDetailsView: error while receiving product: ${err}`);
+  },
+  responseIsNull: () => {
+    return new Error('Error in CatDetailsView: response is null.');
+  },
+  nameNotExist: () => {
+    return new Error('Error in CatDetailsView: name in en-US locale does not exist.');
+  },
+  imagesNotExist: () => {
+    return new Error('Error in CatDetailsView: images of masterVariant does not exist.');
+  },
+  descriptionNotExist: () => {
+    return new Error('Error in CatDetailsView: description in en-US locale does not exist.');
+  },
+};
+
 export default class CatDetailsView extends View {
+  private errors: CatDetailsViewErrors;
+
   private readonly productId: string;
 
   private response: ClientResponse<ProductProjectionPagedQueryResponse> | null;
+
+  private nameEnUS: string | null;
+
+  private imagesObjectsArr: Image[] | null;
+
+  private descriptionEnUS: string | null;
 
   constructor(id: string) {
     const params: ISource = {
@@ -20,26 +54,61 @@ export default class CatDetailsView extends View {
     };
     super(params);
 
+    this.errors = Errors;
+
     this.productId = id;
+
     this.response = null;
 
+    this.nameEnUS = null;
+    this.imagesObjectsArr = null;
+    this.descriptionEnUS = null;
+
     this.getResponseWithProduct().then(
-      () => {
+      (response) => {
+        this.response = response;
+        this.saveName(this.response);
+        this.saveImagesObjectsArr(this.response);
+        this.saveDescription(this.response);
         this.configureView().then(() => {});
       },
       (err) => {
-        throw new Error(`Error in CatDetailsView: error while receiving product: ${err}`);
+        throw this.errors.getResponseError(err);
       },
     );
   }
 
-  private async getResponseWithProduct(): Promise<void> {
-    this.response = await new Product().getProduct(this.productId);
+  private saveName(response: ClientResponse<ProductProjectionPagedQueryResponse>): void {
+    if (response.body.results[0].name && response.body.results[0].name['en-US']) {
+      this.nameEnUS = response.body.results[0].name['en-US'];
+    } else {
+      throw this.errors.nameNotExist();
+    }
+  }
+
+  private saveImagesObjectsArr(response: ClientResponse<ProductProjectionPagedQueryResponse>): void {
+    if (response.body.results[0].masterVariant.images) {
+      this.imagesObjectsArr = response.body.results[0].masterVariant.images;
+    } else {
+      throw this.errors.imagesNotExist();
+    }
+  }
+
+  private saveDescription(response: ClientResponse<ProductProjectionPagedQueryResponse>): void {
+    if (response.body.results[0].description && response.body.results[0].description['en-US']) {
+      this.descriptionEnUS = response.body.results[0].description['en-US'];
+    } else {
+      throw this.errors.descriptionNotExist();
+    }
+  }
+
+  private async getResponseWithProduct(): Promise<ClientResponse<ProductProjectionPagedQueryResponse>> {
+    return new Product().getProduct(this.productId).then((response) => response);
   }
 
   private async configureView(): Promise<void> {
     if (this.response === null) {
-      throw new Error(`Error in CatDetailsView: response is null.`);
+      throw this.errors.responseIsNull();
     }
 
     const catImgParams = {
