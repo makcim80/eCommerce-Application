5de1f376c64b7a424e4fe28da82b80ff3eb5e641
Darya Kondrina <dashakondrina61299@gmail.com>
2023-09-01 19:49:35 +0400

    refactor: put into separate file class Errors

--- src/app/view/main/cat-details/cat-details-view.ts
+++ src/app/view/main/cat-details/cat-details-view.ts
@@ -6,44 +6,7 @@ import View from '../../view';
 import Product from '../../../../components/product';
 import ElementCreator from '../../../util/element-creator';
 import { ListAttributes } from '../../../util/enums/list-attributes';
-
-interface CatDetailsViewErrors {
-  getResponseError: (err: Error) => Error;
-  generalClassError: (err: Error) => Error;
-  responseIsNull: () => Error;
-  nameNotExist: () => Error;
-  imagesNotExist: () => Error;
-  descriptionNotExist: () => Error;
-  currencyCodeNotExist: () => Error;
-  priceDefaultNotExist: () => Error;
-}
-
-const Errors: CatDetailsViewErrors = {
-  getResponseError: (err) => {
-    return new Error(`Error in CatDetailsView: error while receiving product: ${err}`);
-  },
-  generalClassError: (err) => {
-    return new Error(`Error in CatDetailsView: ${err}`);
-  },
-  responseIsNull: () => {
-    return new Error('Error in CatDetailsView: response is null.');
-  },
-  nameNotExist: () => {
-    return new Error('Error in CatDetailsView: name in en-US locale does not exist.');
-  },
-  imagesNotExist: () => {
-    return new Error('Error in CatDetailsView: images of masterVariant does not exist.');
-  },
-  descriptionNotExist: () => {
-    return new Error('Error in CatDetailsView: description in en-US locale does not exist.');
-  },
-  currencyCodeNotExist: () => {
-    return new Error('Error in CatDetailsView: price object or currency code does not exist.');
-  },
-  priceDefaultNotExist: () => {
-    return new Error('Error in CatDetailsView: price object or price in cents does not exist.');
-  },
-};
+import Errors, { CatDetailsViewErrors } from './utils/errors';
 
 export default class CatDetailsView extends View {
   private errors: CatDetailsViewErrors;
