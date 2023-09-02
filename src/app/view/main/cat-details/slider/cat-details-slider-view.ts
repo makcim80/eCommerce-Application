8869c1c043640906989a466f5c1797402978d62d
Darya Kondrina <dashakondrina61299@gmail.com>
2023-09-02 22:19:25 +0400

    feat: incapsulate slider placeholder code into separate method, add initSwiper method

--- src/app/view/main/cat-details/slider/cat-details-slider-view.ts
+++ src/app/view/main/cat-details/slider/cat-details-slider-view.ts
@@ -1,4 +1,11 @@
 import { Image } from '@commercetools/platform-sdk';
+// <editor-fold desc="swiper imports">
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+// </editor-fold desc="swiper imports">
 import { ISource } from '../../../../util/types';
 import { ListTags } from '../../../../util/enums/list-tags';
 import { ListClasses } from '../../../../util/enums/list-classes';
@@ -9,24 +16,32 @@ import View from '../../../view';
 export default class CatDetailsSliderView extends View {
   private imagesObjectsArr: Image[];
 
+  private swiper: Swiper | null;
+
   constructor(imagesObjectsArr: Image[]) {
     const CatDetailsSliderParams: ISource = {
       tag: ListTags.CONTAINER,
-      classNames: ListClasses.CAT_DETAILS_SLIDER,
+      classNames: ListClasses.CAT_DETAILS_SLIDER_TMP,
     };
     super(CatDetailsSliderParams);
 
     this.imagesObjectsArr = imagesObjectsArr;
+    this.swiper = null;
 
     this.configureView();
+    this.initSwiper();
   }
 
   private configureView(): void {
+    this.initSliderPlaceholder();
+  }
+
+  private initSliderPlaceholder(): void {
     this.imagesObjectsArr.forEach((imgObj) => {
       console.log(imgObj);
       const catImgParams: ISource = {
         tag: ListTags.IMG,
-        classNames: ListClasses.CAT_DETAILS_IMG,
+        classNames: ListClasses.CAT_DETAILS_IMG_TMP,
       };
       const catImg = new ElementCreator(catImgParams);
 
@@ -34,5 +49,17 @@ export default class CatDetailsSliderView extends View {
 
       this.view.addInnerElement(catImg);
     });
+  }
+
+  private initSwiper(): void {
+    // init Swiper:
+    this.swiper = new Swiper('.swiper', {
+      // configure Swiper to use modules
+      modules: [Navigation, Pagination],
+    });
+
+    this.swiper.allowSlideNext = true;
+    // Display swiper container HTMLElement.
+    console.log(this.swiper.el);
   }
 }
