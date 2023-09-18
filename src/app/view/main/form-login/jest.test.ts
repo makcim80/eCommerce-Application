c7500154e3f4df8acf28ed60e3d7e2eebc9be329
Yuliya Shelenhouskaya <yulchik241@mail.ru>
2023-09-18 21:36:39 +0300

    fix: delete commits

--- src/app/view/main/form-login/jest.test.ts
+++ src/app/view/main/form-login/jest.test.ts
@@ -1,11 +1,5 @@
-// import App from '../../../app';
-// import Router from '../../../router/router';
 import EmailView from './login-email-view';
 import PasswordView from './login-password-view';
-// import LoginView from './login-view';
-
-// const app = new App();
-// const routes = app.createRoutes();
 
 describe('test', () => {
   it('should check if a value is true', () => {
@@ -24,9 +18,4 @@ describe('test', () => {
     const elem = new PasswordView();
     expect(elem.validationPassword());
   });
-  // it('should check if property exist', () => {
-  //   const router = new Router(routes);
-  //   const elem = new LoginView(router);
-  //   expect(elem).toHaveProperty(['constructor']);
-  // });
 });
